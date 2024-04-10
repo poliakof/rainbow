a94a0febd28e9a8b5bdb6bd122d98772314b672f
poliakof <161254668+poliakof@users.noreply.github.com>
2024-04-10 03:54:52 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function changeColor() { 
+    let red = Math.floor(Math.random() * 256); 
+    let green = Math.floor(Math.random() * 256); 
+    let blue = Math.floor(Math.random() * 256); 
+    let color = "rgb(" + red + "," + green + "," + blue + ")"; 
+    document.getElementById("rainbow").style.backgroundColor = color; 
+}
